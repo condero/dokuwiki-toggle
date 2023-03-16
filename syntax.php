13b474db230c9f38c037e6cd54822523638e986e
Vicente de Andrade <vicentedeandrade7@gmail.com>
2023-03-16 16:51:04 -0300

    Update syntax.php
    Work in recent version of DokuWiki, in my case it's 2022-07-31a "Igor".

--- syntax.php
+++ syntax.php
@@ -58,7 +58,7 @@ class syntax_plugin_toggle extends DokuWiki_Syntax_Plugin {
 	/**
 	 * Handle the match
 	 */
-	function handle($match, $state, $pos, &$handler){
+	public function handle($match, $state, $pos, Doku_Handler $handler){
 		switch ($state) {
 			case DOKU_LEXER_ENTER : 
 				return array($state, '');
@@ -76,7 +76,7 @@ class syntax_plugin_toggle extends DokuWiki_Syntax_Plugin {
 	/**
 	 * Create output
 	 */
-	function render($mode, &$renderer, $data) {
+	public function render($mode, Doku_Renderer $renderer, $data) {
 		if($mode == 'xhtml'){
 			list($state, $match) = $data;
 			switch ($state) {
